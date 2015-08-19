a37704877d0d0a842a43c79acb9880f61f9870b8
MPereira <mpereira132@hotmail.com>
2015-08-19 21:16:46 +0100

    #S01

---
+++ app.js
@@ -0,0 +1,69 @@
+var app = angular.module("App", ['ngRoute','ngAnimate']);
+
+
+app
+.config(['$routeProvider',
+	function($routeProvider) {
+	    $routeProvider.
+	     when('/home', {
+	        templateUrl: 'views/home.html',
+	        controller: 'HomeController'
+	      }).
+	      when('/settings', {
+	        templateUrl: 'views/settings.html',
+	        controller: 'SettingsController'
+	      }).
+	      when('/view2', {
+	        templateUrl: 'views/view2.html',
+	        controller: 'View2Controller'
+	      }).
+	      otherwise({
+	        redirectTo: '/home'
+	      });
+	  }
+	])
+
+
+.directive('navbar', function() {
+return {
+    restrict: 'EA',
+    scope: {
+    	sideBarOpenned: '=open'
+	},
+    templateUrl: 'shared/navbar/navbar.html',
+
+    link: function (scope, element, attrs) {
+
+    		//scope.sideBarOpenned = false;
+			scope.selectedOpt = 1;
+
+
+
+            // now do stuff with the number, you can access it through the scope
+            scope.navUrl = function ( url ){
+            	//$location.path(path);
+            }
+            
+            //Side Bar Behavior
+            scope.sideBarOpen = function ( Opt ){
+            	if(scope.sideBarOpenned){
+            		if(scope.selectedOpt != Opt){
+            			scope.selectedOpt = Opt;
+            			scope.sideBarOpenned = true;
+						}
+            		else
+            			scope.sideBarOpenned = false;
+            	}else{
+            		scope.selectedOpt = Opt;
+            		scope.sideBarOpenned = true;
+            	}
+
+
+            }
+
+        }
+  };
+
+
+
+});
---
+++ js/controllers/HomeController.js
@@ -0,0 +1,28 @@
+app
+.controller('HomeController', ['$scope', '$location', function($scope, $location) { 
+	$scope.move = false;
+
+	$scope.navigate = function (path) {
+		$location.path(path);
+	};
+
+}])
+
+
+
+.controller('SettingsController', ['$scope', '$location', function($scope, $location) { 
+
+	
+	
+	
+}])
+
+
+.controller('View2Controller', ['$scope', '$location', function($scope, $location) { 
+
+	
+		
+	
+}]);
+
+
